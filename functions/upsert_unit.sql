6fa8e908174e9511addf96300b10600a2323aa65
skye-lopez <sky.lopez.dev@gmail.com>
2024-08-06 11:53:40 -0700

    upsert_unit function

--- functions/upsert_unit.sql
+++ functions/upsert_unit.sql
@@ -3,12 +3,19 @@ create or replace function upsert_unit (
     g_name text,
     placement integer
 ) returns void as $$
-declare r record;
+declare 
+r record;
+placements integer[];
 begin
     select * from units where id = g_id into r;
-    if (r.id is not null) then 
-        -- Create new record
---      
+    if (r.id is null) then 
+        placements = '{0,0,0,0,0,0,0,0}'::integer[];
+        placements[placement] := 1;
+        insert into units (id, name, places) values (g_id, g_name, placements);
+    else
+        placements := r.places;
+        placements[placement] := (placements[placement] + 1);
+        update units set places = placements where id = g_id;
     end if;
 end
 $$ language plpgsql;
